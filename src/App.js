d59eabe52105319364a2cbe5492290c95be73786
Mohan Raj <mohanraj@archimedis.net>
2022-11-01 19:28:02 +0530

    My First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+// import LoginForm from './LoginForm';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import SignUp from './SignUp';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+   return <SignUp></SignUp>;
+}
+
+export default App;
